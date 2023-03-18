7b03a05d966262fe29c2cb90cadee1122ccec209
Jubril Somide <jbsomide@gmail.com>
2023-03-18 16:11:42 -0600

    Added profit margins to part 1

---
+++ part1.py
@@ -0,0 +1,7 @@
+profit_margins = {1: 120.45,
+                  2: 99.50,
+                  3: 75.69,
+                  4: 65.73,
+                  5: 51.49}
+
+print("Welcome to Circle Phones' Profit calculator.")
